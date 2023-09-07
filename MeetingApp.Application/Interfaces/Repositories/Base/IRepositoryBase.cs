7024293bf5e2bf7f032143da0b054bc8a6fde681
Melisa Şahin <111955439+melisasahinn@users.noreply.github.com>
2023-09-07 22:52:50 +0300

    repository added

--- MeetingApp.Application/Interfaces/Repositories/Base/IRepositoryBase.cs
+++ MeetingApp.Application/Interfaces/Repositories/Base/IRepositoryBase.cs
@@ -11,13 +11,21 @@ namespace MeetingApp.Application.Interfaces.Repositories.Base
     public interface IRepositoryBase<TEntity> where TEntity : BaseEntity
     {
         TEntity Entity { get; set; }
-        Task<TEntity> GetByIdAsync(long id);
-        //Task<List<TEntity>> GetAll(DataFilterModel dataFilterModel);
-        Task<TEntity> AddAsync(TEntity entity, bool IsSaveChanges = true);
-        Task Delete(long id);
-        Task<TEntity> Update(TEntity entity, bool IsSaveChanges = true);
-        Task<TEntity> GetFirstOrDefaultAsync(Expression<Func<TEntity, bool>> filter);
-        Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter);
-        Task SaveChangesAsync();
+        TEntity GetById(long id, bool showDeleted = false);
+        //IQueryable<TEntity> GetAll(DataFilterModel dataFilterModel);
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity, bool deletion = false);
+        TEntity GetFirstOrDefault(Expression<Func<TEntity, bool>> filter);
+
+        //TEntity Entity { get; set; }
+        //Task<TEntity> GetByIdAsync(long id);
+        ////Task<List<TEntity>> GetAll(DataFilterModel dataFilterModel);
+        //Task<TEntity> AddAsync(TEntity entity, bool IsSaveChanges = true);
+        //Task Delete(long id);
+        //Task<TEntity> Update(TEntity entity, bool IsSaveChanges = true);
+        //Task<TEntity> GetFirstOrDefaultAsync(Expression<Func<TEntity, bool>> filter);
+        //Task<List<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter);
+        //Task SaveChangesAsync();
     }
 }
